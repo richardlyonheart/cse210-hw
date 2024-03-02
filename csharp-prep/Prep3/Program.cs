dc9fd214a0252d29a302fdf6d19da19549496478
richardlyonheart <brendenlyon907@gmail.com>
2024-03-02 16:12:22 -0600

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        
+        //Console.WriteLine("What is the magic number? ");
+        //string magicNumber = Console.ReadLine();
+        //int ans = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int ans = randomGenerator.Next(1,51);
+
+        int numguess = -1;
+        while (numguess != ans)
+
+       {
+            Console.WriteLine("Guess a number between 1 and 50? ");
+            string guess = Console.ReadLine();
+            numguess = int.Parse(guess);
+
+            if (numguess == ans)
+            {
+            Console.WriteLine("Good guessing youre right on!");
+            }
+            else if (numguess < ans)
+            {
+            Console.WriteLine("You need to guess Higher.");
+            }
+            else if (numguess > ans)
+            {
+            Console.WriteLine("You need to guess Lower.");
+            }
+       } 
     }
 }

39015a3f0d24794d90793ba3d01876755fcf2132
richardlyonheart <brendenlyon907@gmail.com>
2024-03-15 17:17:47 -0500

    fixes

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -3,10 +3,6 @@ using System.Reflection.Metadata.Ecma335;
 public class Reference
 {
     private string _reference;
-    private string _book;
-    private int _chapter;
-    private int _verse;
-    private int _endVerse;
 
     public Reference(string reference)
     {

773b64b05d155f55ce33da308046bd95ba374374
richardlyonheart <brendenlyon907@gmail.com>
2024-03-20 11:38:18 -0500

    begining learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Brenden Lyon", "CSE-210");
+        Console.WriteLine(assignment1.GetSummary());
     }
 }

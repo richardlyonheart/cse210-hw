52381d88336098c8f241019533a705a7bce8eb26
richardlyonheart <brendenlyon907@gmail.com>
2024-03-28 20:52:11 -0500

    learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Green", 7);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 2, 3);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
